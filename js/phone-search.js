672ce0dda41489f521b937330b858a1fa194321c
emdad118661 <emdad118661@gmail.com>
2022-03-02 18:24:48 +0600

    search result dynamically

--- js/phone-search.js
+++ js/phone-search.js
@@ -7,7 +7,30 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data.slice(0, 20)));
 
     // console.log(url);
+
+    const displaySearchResult = data => {
+        // console.log(data);
+        const searchResult = document.getElementById('search-result');
+        data.forEach(phone => {
+            console.log(phone);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Brand: ${phone.brand}<br>
+                    Phone Name: ${phone.phone_name}</h5>
+                    <p class="card-text">${phone.slug}</p>
+                    <button type="button" class="btn btn-primary">Details</button>
+                </div>
+            </div>
+            `;
+            searchResult.appendChild(div);
+        })
+    }
 }
+
